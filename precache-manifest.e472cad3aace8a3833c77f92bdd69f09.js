a2184ba7785dea17cea5f199e12e5c95cd41a2c2
Boriss Nazarovs <boriss.nazarovs@gmail.com>
2018-10-21 14:30:14 +0300

    Updates

---
+++ precache-manifest.e472cad3aace8a3833c77f92bdd69f09.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "babc4c7ede60f081e69c",
+    "url": "/mfst/docs/static/js/runtime~main.babc4c7e.js"
+  },
+  {
+    "revision": "50a624e5a361b61e795e",
+    "url": "/mfst/docs/static/js/main.50a624e5.chunk.js"
+  },
+  {
+    "revision": "42026eb7691d98de6ed5",
+    "url": "/mfst/docs/static/js/1.42026eb7.chunk.js"
+  },
+  {
+    "revision": "be614d00bf754577b454fd464a8bca89",
+    "url": "/mfst/docs/index.html"
+  }
+];
